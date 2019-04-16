261e9a809b61bbd9012a4ec4dc599c24b798e8f3
zehrabetulboynuegri <zehrabetulboynuegri@gmail.com>
2019-04-16 18:39:08 +0300

    Template Integration

--- src/main/java/com/pasvak/asevi/controller/HomeController.java
+++ src/main/java/com/pasvak/asevi/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.pasvak.asevi.controller;
 
 
+import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
 
+@Controller
+public class HomeController {
+/*
     @RequestMapping("/")
     public String home(){
         return "Hello World!";
+    }
+  */
+	   @RequestMapping("/")
+	    public String index() {
+	        return "index";
+	    }
+	   @RequestMapping("/plain-page")
+    public String plainPage() {
+        return "plain-page";
+    }
+
+	   @RequestMapping("/pricing-tables")
+    public String pricingTables() {
+        return "pricing-tables";
     }
 }
